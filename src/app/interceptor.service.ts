64c44030b383ae19efa8a82bdeb13357e149a2a8
Emmanuel <d.emmanuel.duque@gmail.com>
2021-03-05 07:58:48 +0100

    interceptor

--- src/app/interceptor.service.ts
+++ src/app/interceptor.service.ts
@@ -1,21 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import {
   HttpErrorResponse,
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
-} from '@angular/common/http';
-import { Observable, of, throwError } from 'rxjs';
-import { catchError, delay, mergeMap, retry, retryWhen } from 'rxjs/operators';
-import { Router } from '@angular/router';
+} from "@angular/common/http";
+import { Observable, of, throwError } from "rxjs";
+import { catchError, delay, mergeMap, retry, retryWhen } from "rxjs/operators";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class GlobalHttpInterceptorService implements HttpInterceptor {
-
-  constructor(public router: Router) { }
-
-
+  constructor(public router: Router) {}
 
   intercept(
     req: HttpRequest<any>,
@@ -24,69 +21,57 @@ export class GlobalHttpInterceptorService implements HttpInterceptor {
     return next.handle(req).pipe(
       catchError((error) => {
         if (error instanceof HttpErrorResponse) {
-          if (
-            error.status === 401 ||
-            error.status === 500
-          ) {
+          if (error.status === 404 || error.status === 500) {
             return next.handle(req).pipe(
-              retryWithBackOff(),
+              retryControllForAuthErrors(),
               catchError((error: HttpErrorResponse) => {
-                let errorMessage = `[ARGO] Server-side error arquitecture: ${error.status} ${error.message}`;
+                let errorMessage = `Interceptor Error: ${error.status} ${error.message}`;
                 console.warn(errorMessage);
                 return throwError(error);
               })
-            )
-          }
-          else {
-            console.warn(`[ARGO] Server-side error: ${error.status} ${error.message}`);
+            );
+          } else {
+            //console.warn(`Interceptor Error: ${error.status} ${error.message}`);
             return throwError(error.message);
           }
         }
       })
-    )
+    );
   }
 }
-
 
 const DEFAULT_DELAY = 1000;
 const DEFAULT_MAX_RETRIES = 2;
 const DEFAULT_BACKOFF = 1000;
 
 /**
-* we increase the delay after each retry.
-* The first retry happens after one second, the second one after two seconds
-* and the third one after three seconds.
-* @param delayMs Delay retry
-* @param maxRetry Http retries
-* @param backoffMs delay after each retry
-*/
-const retryWithBackOff = (
+ * we increase the delay after each retry.
+ * The first retry happens after one second, the second one after two seconds
+ * and the third one after three seconds.
+ * @param delayMs Delay retry
+ * @param maxRetry Http retries
+ * @param backoffMs delay after each retry
+ */
+const retryControllForAuthErrors = (
   delayMs: number = DEFAULT_DELAY,
   maxRetry: number = DEFAULT_MAX_RETRIES,
-  backoffMs: number = DEFAULT_BACKOFF) => {
-  let retries = maxRetry;
+  backoffMs: number = DEFAULT_BACKOFF
+) => {
+  let retries = 2;
 
   return (src: Observable<any>) =>
     src.pipe(
-      retryWhen((errors: Observable<any>) => errors.pipe(
-        mergeMap(error => {
-          if (retries --> 0) {
-            console.log('Error en el when');
-
-            const backoffTime = delayMs + (maxRetry - retries) * backoffMs;
-            return of(error).pipe(delay(backoffTime));
-          }
-
-          return throwError(error);
-        }
-        ))
-      ))
-    ;
+      retryWhen((errors: Observable<any>) =>
+        errors.pipe(
+          mergeMap((error) => {
+            if (retries-- > 0) {
+              console.log("Error en el when");
+              const backoffTime = delayMs + (maxRetry - retries) * backoffMs;
+              return of(error).pipe(delay(backoffTime));
+            }
+            return throwError(error);
+          })
+        )
+      )
+    );
 };
-
-
-
-
-
-
-
